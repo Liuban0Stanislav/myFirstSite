db362c55872a2dc827b41391543eeaef79163c06
Liuban0Stanislav <liubanstanislav@yahoo.com>
2023-04-30 17:37:46 +1000

    init commit

--- src/main/java/com/example/myfirstenternetproject/SiteController.java
+++ src/main/java/com/example/myfirstenternetproject/SiteController.java
@@ -1,12 +1,24 @@
 package com.example.myfirstenternetproject;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SiteController {
-    @GetMapping("/main")
+
+    @GetMapping("/")
     public String getMain(){
-        return "main";
+        return "index";
+    }
+    @GetMapping("/contacts")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "contacts";
+    }
+    @GetMapping("/image")
+    public String image(){
+        return "image";
     }
 }
